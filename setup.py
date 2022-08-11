0c534613b24c0b26fe6909b99078fbc8a4f433c9
James Hart <JHART@UK.ibm.com>
2022-08-11 13:10:25 +0100

    fix(project): Travis cannot run python 3.10 yet
    Signed-off-by: James Hart <JHART@UK.ibm.com>

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
